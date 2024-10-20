9023b9a87e0bfdb51c70512ddd2ef16c28647a72
mihai.topa <mihai.topa@stud.acs.upb.ro>
2024-10-20 18:10:52 +0300

    am implementat open - TEST
    Signed-off-by: mihai.topa <mihai.topa@stud.acs.upb.ro>

--- src/io/open.c
+++ src/io/open.c
@@ -7,6 +7,24 @@
 
 int open(const char *filename, int flags, ...)
 {
-	/* TODO: Implement open system call. */
-	return -1;
+	mode_t mode = 0;
+
+	// daca fisierul specificat nu exista, acesta trb creat
+	if (flags & O_CREAT)
+	{
+		va_list args;
+		va_start(args, flags);
+		mode = va_arg(args, mode_t); // permisiunile
+		va_end(args);
+	}
+
+	// dau syscall ca sa apelez openat cu AT_FDCWD ca sa se foloseasca folderul curent
+	int fd = syscall(__NR_openat, AT_FDCWD, filename, flags, mode);
+
+	if (fd == -1)
+	{
+		return -1;
+	}
+
+	return fd;
 }
