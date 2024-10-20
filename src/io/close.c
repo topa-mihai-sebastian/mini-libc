173c0039faa0a26ba5529c26d7efb236c5bdb597
mihai.topa <mihai.topa@stud.acs.upb.ro>
2024-10-20 18:16:58 +0300

    am implementat functia close - TEST
    Signed-off-by: mihai.topa <mihai.topa@stud.acs.upb.ro>

--- src/io/close.c
+++ src/io/close.c
@@ -8,5 +8,9 @@
 int close(int fd)
 {
 	/* TODO: Implement close(). */
-	return -1;
+	int closeRes = syscall(__NR_close, fd);
+
+	if (closeRes == -1)
+		return -1; // fail
+	return 0;	   // succes
 }
