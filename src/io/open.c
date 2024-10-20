1734cd24557b08f3ae76b37b1cd6cb8f78b0e276
mihai.topa <mihai.topa@stud.acs.upb.ro>
2024-10-20 12:58:32 +0300

    bug
    Signed-off-by: mihai.topa <mihai.topa@stud.acs.upb.ro>

--- src/io/open.c
+++ src/io/open.c
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 #include <fcntl.h>
-#include <syscall.h>
-#include "../include/internal/arch/x86_64/syscall_list.h"
+#include <internal/syscall.h>
 #include <stdarg.h>
 #include <errno.h>
 
 int open(const char *filename, int flags, ...)
 {
 	/* TODO: Implement open system call. */
-
 	return -1;
 }
