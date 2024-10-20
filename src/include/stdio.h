f6b49832e1154f50ec328355ee1383ab8a7b9dfa
mihai.topa <mihai.topa@stud.acs.upb.ro>
2024-10-20 09:15:51 +0300

    declarare puts
    Signed-off-by: mihai.topa <mihai.topa@stud.acs.upb.ro>

--- src/include/stdio.h
+++ src/include/stdio.h
@@ -1,14 +1,15 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 
 #ifndef __STDIO_H__
-#define __STDIO_H__	1
+#define __STDIO_H__ 1
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-/* TODO: Add puts() declaration. */
-
+	/* TODO: Add puts() declaration. */
+	int puts(const char *str);
 #ifdef __cplusplus
 }
 #endif
