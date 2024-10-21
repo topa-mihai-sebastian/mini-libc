d5c23a241d51765bdc41cbe117ec6025fec1e0b1
mihai.topa <mihai.topa@stud.acs.upb.ro>
2024-10-21 20:29:46 +0300

    repar time.h
    Signed-off-by: mihai.topa <mihai.topa@stud.acs.upb.ro>

--- src/include/time.h
+++ src/include/time.h
@@ -1,15 +1,22 @@
-// SPDX-License-Identifier: BSD-3-Clause
+/* SPDX-License-Identifier: BSD-3-Clause */
 
-#ifndef TIME_H
-#define TIME_H
-
-struct timespec
+#ifndef __STRING_H__
+#define __STRING_H__ 1
+#ifdef __cplusplus
+extern "C"
 {
-	long tv_sec;
-	long tv_nsec;
-};
+#endif
+#include <internal/types.h>
+	struct timespec
+	{
+		long tv_sec;
+		long tv_nsec;
+	};
+	int nanosleep(const struct timespec *req, struct timespec *rem);
+	unsigned sleep(unsigned seconds);
 
-int nanosleep(const struct timespec *req, struct timespec *rem);
-unsigned sleep(unsigned seconds);
+#ifdef __cplusplus
+}
+#endif
 
 #endif
