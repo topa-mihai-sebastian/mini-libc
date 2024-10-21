54447906ed0806d1c54d3008c8e2e906de011432
mihai.topa <mihai.topa@stud.acs.upb.ro>
2024-10-21 21:44:50 +0300

    functia open
    Signed-off-by: mihai.topa <mihai.topa@stud.acs.upb.ro>

--- src/io/open.c
+++ src/io/open.c
@@ -7,21 +7,9 @@
 
 int open(const char *filename, int flags, ...)
 {
-	mode_t mode = 0;
+	int fd = syscall(__NR_open, filename, flags);
 
-	// daca fisierul specificat nu exista, acesta trb creat
-	if (flags & O_CREAT)
-	{
-		va_list args;
-		va_start(args, flags);
-		mode = va_arg(args, mode_t); // permisiunile
-		va_end(args);
-	}
-	// TEST TEST TEST
-	// dau syscall ca sa apelez openat cu AT_FDCWD ca sa se foloseasca folderul curent
-	int fd = syscall(__NR_openat, AT_FDCWD, filename, flags, mode);
-
-	if (fd == -1)
+	if (fd < 0)
 	{
 		return -1;
 	}
