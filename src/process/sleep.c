16b8b9e6a13be0188ba9b041a46af334c8074405
mihai.topa <mihai.topa@stud.acs.upb.ro>
2024-10-21 19:43:30 +0300

    reparare sleep
    Signed-off-by: mihai.topa <mihai.topa@stud.acs.upb.ro>

--- src/process/sleep.c
+++ src/process/sleep.c
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: BSD-3-Clause
-
 #include <internal/syscall.h>
 #include <internal/io.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <internal/types.h>
-#include "nanosleep.c"
+#include <time.h>
 
 unsigned sleep(unsigned seconds)
 {
