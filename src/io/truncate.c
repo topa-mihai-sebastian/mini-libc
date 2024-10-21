f1f7753b51f7582759d4f01fceb92856b911c51b
mihai.topa <mihai.topa@stud.acs.upb.ro>
2024-10-21 18:52:58 +0300

    implementare truncate - FINAL
    Signed-off-by: mihai.topa <mihai.topa@stud.acs.upb.ro>

--- src/io/truncate.c
+++ src/io/truncate.c
@@ -12,6 +12,20 @@ int truncate(const char *path, off_t length)
 		errno = EINVAL;
 		return -1;
 	}
+
+	struct stat path_stat;
+	if (stat(path, &path_stat) != 0)
+	{
+		errno = ENOENT;
+		return -1;
+	}
+
+	if (S_ISDIR(path_stat.st_mode))
+	{
+		errno = EISDIR;
+		return -1;
+	}
+
 	int final = syscall(__NR_truncate, path, length);
 
 	if (final == -1)
