5976284490c1894393a4865e311d5fa5f8005560
mihai.topa <mihai.topa@stud.acs.upb.ro>
2024-10-22 12:42:37 +0300

    implementare mmap.c
    Signed-off-by: mihai.topa <mihai.topa@stud.acs.upb.ro>

--- src/mm/mmap.c
+++ src/mm/mmap.c
@@ -7,17 +7,36 @@
 void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset)
 {
 	/* TODO: Implement mmap(). */
-	return MAP_FAILED;
+	void *res = syscall(__NR_mmap, addr, prot, flags, fd, offset);
+
+	if (res == MAP_FAILED)
+	{
+		return MAP_FAILED;
+	}
+
+	return res;
 }
 
 void *mremap(void *old_address, size_t old_size, size_t new_size, int flags)
 {
 	/* TODO: Implement mremap(). */
-	return MAP_FAILED;
+	void *res = syscall(__NR_mremap, old_size, new_size, flags);
+
+	if (res == MAP_FAILED)
+	{
+		return MAP_FAILED;
+	}
+
+	return res;
 }
 
 int munmap(void *addr, size_t length)
 {
 	/* TODO: Implement munmap(). */
-	return -1;
+	int res = syscall(__NR_munmap, length);
+	if (res == -1)
+	{
+		return -1;
+	}
+	return 0;
 }
